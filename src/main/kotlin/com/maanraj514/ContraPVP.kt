20545bc4f5ab1d1428bd7b78ab60cf6386d30396
BuildTools <unconfigured@null.spigotmc.org>
2023-08-28 21:07:13 +0800

    First commit on dev branch

--- src/main/kotlin/com/maanraj514/ContraPVP.kt
+++ src/main/kotlin/com/maanraj514/ContraPVP.kt
@@ -1,15 +1,10 @@
 package com.maanraj514
 
-import com.maanraj514.api.util.title
-import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class ContraPVP : JavaPlugin() {
 
     override fun onEnable() {
-        for (player in Bukkit.getOnlinePlayers()) {
-            player.title("", "")
-        }
         println("ContraPVP is enabled")
     }
 
